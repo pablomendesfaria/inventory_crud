85d4e7d9cdd82ae4f976031f4ae46bd7a7b4d800
Pablo Mendes Faria <pablomendesfaria@gmail.com>
2025-01-14 19:43:22 -0300

    configuring the models and updating the database

--- src/models/database.py
+++ src/models/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import sessionmaker
 import os
 
@@ -9,7 +9,9 @@ if not DATABASE_URL:
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    pass
 
 def get_db():
     db = SessionLocal()
---
+++ src/models/models.py
@@ -0,0 +1,57 @@
+from sqlalchemy import Column, Integer, String, Enum, CheckConstraint, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from src.models.database import Base
+from datetime import datetime
+
+class UoMType(Enum):
+    LITRO = "litro"
+    METRO = "metro"
+    QUILOGRAMA = "quilograma"
+    METRO_CUBICO = "metro_cubico"
+    QUANTIDADE = "quantidade"
+
+class MovementType(Enum):
+    ENTRADA = "entrada"
+    SAIDA = "saida"
+
+class Item(Base):
+    __tablename__ = 'items'
+
+    codigo = Column(Integer, primary_key=True, index=True)
+    produto = Column(String, index=True)
+    unidade_medida = Column(Enum(UoMType), nullable=False)
+    custo_medio = Column(Integer, CheckConstraint('custo_medio >= 0', name='average_cost_positive'))
+    valor_venda = Column(Integer, CheckConstraint('valor_venda >= 0', name='sale_value_positive'))
+    estoque = Column(Integer, CheckConstraint('estoque >= 0', name='stock_positive'))
+
+    def to_dict(self):
+        return {
+            'codigo': self.codigo,
+            'produto': self.produto,
+            'unidade_medida': self.unidade_medida.value,
+            'custo_medio': self.custo_medio,
+            'valor_venda': self.valor_venda,
+            'estoque': self.estoque
+        }
+
+class StockMovement(Base):
+    __tablename__ = 'stock_movements'
+
+    id = Column(Integer, primary_key=True, index=True)
+    data = Column(DateTime, default=datetime.now(datetime.timezone.utc), nullable=False)
+    movimentacao = Column(Enum(MovementType), nullable=False)
+    codigo_produto = Column(Integer, ForeignKey('items.codigo'), nullable=False)
+    quantidade = Column(Integer, CheckConstraint('quantidade >= 0', name='quantity_positive'))
+    estoque_final = Column(Integer, CheckConstraint('estoque_final >= 0', name='final_stock_positive'))
+
+    produto = relationship("Item")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'data': self.data.isoformat(),
+            'movimentacao': self.movimentacao.value,
+            'codigo_produto': self.codigo_produto,
+            'quantidade': self.quantidade,
+            'estoque_final': self.estoque_final
+        }
